fb84fec08a0338eeef104cbc8e1312e4ed0958d0
pipietrzyk <pipietrzyk0@gmail.com>
2023-10-24 16:45:38 -0500

    minimax_ab works with some bugs

--- minimax.py
+++ minimax.py
@@ -66,13 +66,52 @@ def min_value(brd : State, plyr, nodes) :
 
 
 
-def minimax_ab(brd, plyr) :
-    cell, nodes = 0
-    return (cell, nodes)
+def minimax_ab(brd : State, plyr) :
+    utility, cell, nodes = max_value_ab(brd, plyr, -10, 10, 1)
+    return cell, nodes
 
-def max_value_ab(brd) :
-    pass
+def max_value_ab(brd : State, plyr, alpha, beta, nodes) :
+    nodes = nodes + 1
 
-def min_value_ab(brd) :
-    pass
+    if plyr == "X" :
+        enemy = "O"
+    else :
+        enemy = "X"
+
+    if brd.is_terminal() :
+        return (brd.utility(plyr), 0, nodes)
+    u = -10
+    for a in brd.actions() :
+        newBrd = State(brd.get_board())
+        newBrd.result(plyr, a-1)
+        u2, a2, nodes = min_value_ab(newBrd, enemy, alpha, beta, nodes)
+        if u2 > u :
+            u, move = u2, a-1
+            alpha = max(a, u)
+        if u >= beta :
+            return u, move, nodes
+    return u, move, nodes
+
+
+def min_value_ab(brd : State, plyr, alpha, beta, nodes) :
+    nodes = nodes + 1
+
+    if plyr == "X" :
+        enemy = "O"
+    else :
+        enemy = "X"
+
+    if brd.is_terminal() :
+        return (brd.utility(enemy), 0, nodes)
+    u = 10
+    for a in brd.actions() :
+        newBrd = State(brd.get_board())
+        newBrd.result(plyr, a-1)
+        u2, a2, nodes = max_value_ab(newBrd, enemy, alpha, beta, nodes)
+        if u2 < u :
+            u, move = u2, a-1
+            beta = min(beta, u)
+        if u <= alpha :
+            return u, move, nodes
+    return u, move, nodes
 
